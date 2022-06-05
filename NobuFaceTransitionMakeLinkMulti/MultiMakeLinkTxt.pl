77d3a9a54fe6e32666d1a5900466167c95ae512d
tenshoukijp <komiyama_a@hotmail.com>
2022-06-06 00:57:03 +0900

    追加

--- NobuFaceTransitionMakeLinkMulti/MultiMakeLinkTxt.pl
+++ NobuFaceTransitionMakeLinkMulti/MultiMakeLinkTxt.pl
@@ -5,7 +5,7 @@ foreach my $curFileName (<nobu15pk_*.png>) {
     print STDERR "$curFileName\n";
     (my $number) = $curFileName =~ m/_(\d+)\.png/;
 	if ($number >= 0) {
-		my $ret = `KaoORBMatching_multi $curFileName $targetFileName`;
+		my $ret = `KaoORBMatching_multi $curFileName`;
 		if ($ret =~ /=>/sm ) {
 			print $ret;
 		}
